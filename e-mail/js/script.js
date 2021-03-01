7ada4451282aa9a0887c70bde2f711533a7f5dd7
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-01 15:28:32 +0100

    Fixes

--- e-mail/js/script.js
+++ e-mail/js/script.js
@@ -1,12 +1,12 @@
 // Dichiarazione lista email
 var emailAutorizzate = [ "alessio@boolean.it", "chiara@boolean.it", "clelia@boolean.it", "margherita@boolean.it", "marta@boolean.it", "samuele@boolean.it", "valeria@boolean.it", "veronica@boolean.it" ],
 // Dichiarazione variabili
-userEmail = prompt("Inserisci la tua e-mail"),
+userEmail = prompt("Inserisci la tua e-mail").toLowerCase(),
 autorizzazione = false;
 
 // Ciclo che verifica se userEmail corrisponde ad una delle email autorizzate e in tal caso restituisce "autorizzazione = true"
 for ( var i = 0; i < emailAutorizzate.lenght; i++ ); {
-  if ( emailAutorizzate[i] === userEmail ) {
+  if ( emailAutorizzate[i] == userEmail ) {
     autorizzazione = true;
     i = emailAutorizzate.lenght;
   }
@@ -22,11 +22,11 @@ if ( autorizzazione == true ) {
 
 // // OOPURE condensato
 // var emailAutorizzate = [ "alessio@boolean.it", "chiara@boolean.it", "clelia@boolean.it", "margherita@boolean.it", "marta@boolean.it", "samuele@boolean.it", "valeria@boolean.it", "veronica@boolean.it" ],
-// userEmail = prompt("Inserisci la tua e-mail");
+// userEmail = prompt("Inserisci la tua e-mail").toLowerCase();
 //
 // // Ciclo e messaggio output
 // for ( var i = 0; i < emailAutorizzate.lenght; i++ ); {
-//   if ( emailAutorizzate[i] === userEmail ) {
+//   if ( emailAutorizzate[i] == userEmail ) {
 //     i = emailAutorizzate.lenght;
 //     alert("L' email ha l' autorizzazione ad accedere");
 //   } else if ( i = emailAutorizzate.lenght ) {
